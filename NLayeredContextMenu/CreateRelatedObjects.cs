0d7af33027d512aeab4e48d83854a6cdd5121661
halil ibrahim duymaz <halildymz16@hotmail.com>
2020-03-02 18:00:17 +0300

    Seperated Business file operations from Main

--- NLayeredContextMenu/CreateRelatedObjects.cs
+++ NLayeredContextMenu/CreateRelatedObjects.cs
@@ -21,26 +21,11 @@ namespace NLayeredContextMenu
     /// </summary>
     internal sealed class CreateRelatedObjects
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
+       
         public const int CommandId = 0x0100;
-
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
         public static readonly Guid CommandSet = new Guid("d1103015-8d0f-4e7a-be44-465e8893d23e");
-
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
         private readonly AsyncPackage package;
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CreateRelatedObjects"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
+      
         private CreateRelatedObjects(AsyncPackage package, OleMenuCommandService commandService)
         {
             this.package = package ?? throw new ArgumentNullException(nameof(package));
@@ -51,18 +36,13 @@ namespace NLayeredContextMenu
             commandService.AddCommand(menuItem);
         }
 
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
         public static CreateRelatedObjects Instance
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
+      
         private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
         {
             get
@@ -77,10 +57,7 @@ namespace NLayeredContextMenu
                 return package;
             }
         }
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
+   
         public static async Task InitializeAsync(AsyncPackage package)
         {
             // Switch to the main thread - the call to AddCommand in CreateRelatedObjects's constructor requires
@@ -91,13 +68,7 @@ namespace NLayeredContextMenu
             Instance = new CreateRelatedObjects(package, commandService);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
+       
         private void Execute(object sender, EventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
@@ -154,7 +125,7 @@ namespace NLayeredContextMenu
                                 var fileParameters = new CreateFileParameters
                                 {
                                     ProjectItem = item,
-                                    ProjectTemplate=projectTemplate,
+                                    ProjectTemplate = projectTemplate,
                                     ProjectName = project.Name,
                                     FileNameWithoutExtension = fileNameWithoutExtension,
 
@@ -199,11 +170,11 @@ namespace NLayeredContextMenu
                                 };
                                 if (item.Name == "Abstract")
                                 {
-                                    CreateBusinessAbstract(fileParameters);
+                                    BusinessFileService.CreateBusinessAbstract(fileParameters);
                                 }
                                 if (item.Name == "Concrete")
                                 {
-                                    CreateBusinessConcrete(fileParameters);
+                                    BusinessFileService.CreateBusinessConcrete(fileParameters);
                                 }
                             }
                         }
@@ -240,135 +211,11 @@ namespace NLayeredContextMenu
             return false;
         }
 
-      
-        
-
-        private void CreateBusinessAbstract(CreateFileParameters fileParameters)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            try
-            {
-                var addedItem = fileParameters.ProjectItem.ProjectItems.AddFromTemplate(fileParameters.ProjectTemplate,
-                                                                        $"I{fileParameters.FileNameWithoutExtension}Service.cs");
-
-                var addedItemDocument = addedItem.Document;
-                var textDocument = addedItemDocument.Object() as TextDocument;
-                var p = textDocument.StartPoint.CreateEditPoint();
-                p.Delete(textDocument.EndPoint);
-                p.Insert(CreateBusinessAbstractFileContent(fileParameters.FileNameWithoutExtension, fileParameters.ProjectName));
-                p.SmartFormat(textDocument.StartPoint);
-                addedItemDocument.Save();
-            }
-            catch
-            {
-                throw;
-            }
-        }
-        private void CreateBusinessConcrete(CreateFileParameters fileParameters)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            try
-            {
-                var addedItem = fileParameters.ProjectItem.ProjectItems.AddFromTemplate(fileParameters.ProjectTemplate, $"{fileParameters.FileNameWithoutExtension}Manager.cs");
-                var addedItemDocument = addedItem.Document;
-                var textDocument = addedItemDocument.Object() as TextDocument;
-                var p = textDocument.StartPoint.CreateEditPoint();
-                p.Delete(textDocument.EndPoint);
-                p.Insert(CreateBusinessConcreteFileContent(fileParameters.FileNameWithoutExtension, fileParameters.ProjectName));
-                p.SmartFormat(textDocument.StartPoint);
-                addedItemDocument.Save();
-            }
-            catch
-            {
-                throw;
-            }
-        }
-
         private bool DoesProjectFolderExists(string projectFullName, string folderNameToCheck)
         {
             var projectPath = Path.GetDirectoryName(projectFullName);
 
             return Directory.Exists(Path.Combine(projectPath, folderNameToCheck));
-        }
-        
-       
-
-        private string CreateBusinessAbstractFileContent(string fileName, string projectName)
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("using System;");
-            stringBuilder.AppendLine("using Entities.Concrete;");
-            stringBuilder.AppendLine("using System.Collections.Generic;");
-            stringBuilder.AppendLine("\n");
-            stringBuilder.AppendLine($"namespace {projectName}.Abstract");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"public interface I{fileName}Service");
-            stringBuilder.AppendLine("{");
-
-            stringBuilder.AppendLine($"{fileName} Get(int id);");
-            stringBuilder.AppendLine($"List<{fileName}> GetList();");
-            stringBuilder.AppendLine($"void Add({fileName} entity);");
-            stringBuilder.AppendLine($"void Update({fileName} entity);");
-            stringBuilder.AppendLine($"void Delete(int id);");
-
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine("}");
-
-            return stringBuilder.ToString();
-
-        }
-        private string CreateBusinessConcreteFileContent(string fileName, string projectName)
-        {
-            string camelCasedFileName = char.ToLowerInvariant(fileName[0]) + fileName.Substring(1);
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("using System;");
-            stringBuilder.AppendLine("using System.Linq;");
-            stringBuilder.AppendLine("using System.Collections.Generic;");
-            stringBuilder.AppendLine("using Entities.Concrete;");
-            stringBuilder.AppendLine("using DataAccess.Abstract;");
-            stringBuilder.AppendLine($"using {projectName}.Abstract;");
-            stringBuilder.AppendLine("\n");
-            stringBuilder.AppendLine($"namespace {projectName}.Concrete");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"public class {fileName}Manager:I{fileName}Service");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"private I{fileName}Dal _{camelCasedFileName}Dal;");
-            stringBuilder.AppendLine($"public {fileName}Manager(I{fileName}Dal {camelCasedFileName}Dal)");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"_{camelCasedFileName}Dal={camelCasedFileName}Dal;");
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine($"public {fileName} Get(int id)");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"return _{camelCasedFileName}Dal.Get(id);");
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine($"public List<{fileName}> GetList()");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"return _{camelCasedFileName}Dal.GetList();");
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine($"public void Add({fileName} entity)");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"_{camelCasedFileName}Dal.Add(entity);");
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine($"public void Update({fileName} entity)");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"_{camelCasedFileName}Dal.Update(entity);");
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine($"public void Delete(int id)");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"_{camelCasedFileName}Dal.Delete(id);");
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine("}");
-
-            stringBuilder.AppendLine("}");
-
-            return stringBuilder.ToString();
         }
 
     }
---
+++ NLayeredContextMenu/Services/BusinessFileService.cs
@@ -0,0 +1,139 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using NLayeredContextMenu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayeredContextMenu.Services
+{
+    public static class BusinessFileService
+    {
+        #region Abstract
+
+        public static void CreateBusinessAbstract(CreateFileParameters fileParameters)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                var addedItem = fileParameters.ProjectItem.ProjectItems.AddFromTemplate(fileParameters.ProjectTemplate,
+                                                                        $"I{fileParameters.FileNameWithoutExtension}Service.cs");
+
+                var addedItemDocument = addedItem.Document;
+                var textDocument = addedItemDocument.Object() as TextDocument;
+                var p = textDocument.StartPoint.CreateEditPoint();
+                p.Delete(textDocument.EndPoint);
+                p.Insert(CreateBusinessAbstractFileContent(fileParameters.FileNameWithoutExtension, fileParameters.ProjectName));
+                p.SmartFormat(textDocument.StartPoint);
+                addedItemDocument.Save();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private static string CreateBusinessAbstractFileContent(string fileName, string projectName)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("using System;");
+            stringBuilder.AppendLine("using Entities.Concrete;");
+            stringBuilder.AppendLine("using System.Collections.Generic;");
+            stringBuilder.AppendLine("\n");
+            stringBuilder.AppendLine($"namespace {projectName}.Abstract");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"public interface I{fileName}Service");
+            stringBuilder.AppendLine("{");
+
+            stringBuilder.AppendLine($"{fileName} Get(int id);");
+            stringBuilder.AppendLine($"List<{fileName}> GetList();");
+            stringBuilder.AppendLine($"void Add({fileName} entity);");
+            stringBuilder.AppendLine($"void Update({fileName} entity);");
+            stringBuilder.AppendLine($"void Delete(int id);");
+
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine("}");
+
+            return stringBuilder.ToString();
+
+        }
+        #endregion
+
+        #region Concrete
+        public static void CreateBusinessConcrete(CreateFileParameters fileParameters)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                var addedItem = fileParameters.ProjectItem.ProjectItems.AddFromTemplate(fileParameters.ProjectTemplate, $"{fileParameters.FileNameWithoutExtension}Manager.cs");
+                var addedItemDocument = addedItem.Document;
+                var textDocument = addedItemDocument.Object() as TextDocument;
+                var p = textDocument.StartPoint.CreateEditPoint();
+                p.Delete(textDocument.EndPoint);
+                p.Insert(CreateBusinessConcreteFileContent(fileParameters.FileNameWithoutExtension, fileParameters.ProjectName));
+                p.SmartFormat(textDocument.StartPoint);
+                addedItemDocument.Save();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static string CreateBusinessConcreteFileContent(string fileName, string projectName)
+        {
+            string camelCasedFileName = char.ToLowerInvariant(fileName[0]) + fileName.Substring(1);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("using System;");
+            stringBuilder.AppendLine("using System.Linq;");
+            stringBuilder.AppendLine("using System.Collections.Generic;");
+            stringBuilder.AppendLine("using Entities.Concrete;");
+            stringBuilder.AppendLine("using DataAccess.Abstract;");
+            stringBuilder.AppendLine($"using {projectName}.Abstract;");
+            stringBuilder.AppendLine("\n");
+            stringBuilder.AppendLine($"namespace {projectName}.Concrete");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"public class {fileName}Manager:I{fileName}Service");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"private I{fileName}Dal _{camelCasedFileName}Dal;");
+            stringBuilder.AppendLine($"public {fileName}Manager(I{fileName}Dal {camelCasedFileName}Dal)");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"_{camelCasedFileName}Dal={camelCasedFileName}Dal;");
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine($"public {fileName} Get(int id)");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"return _{camelCasedFileName}Dal.Get(id);");
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine($"public List<{fileName}> GetList()");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"return _{camelCasedFileName}Dal.GetList();");
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine($"public void Add({fileName} entity)");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"_{camelCasedFileName}Dal.Add(entity);");
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine($"public void Update({fileName} entity)");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"_{camelCasedFileName}Dal.Update(entity);");
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine($"public void Delete(int id)");
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine($"_{camelCasedFileName}Dal.Delete(id);");
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine("}");
+
+            stringBuilder.AppendLine("}");
+
+            return stringBuilder.ToString();
+        }
+        #endregion
+    }
+}
