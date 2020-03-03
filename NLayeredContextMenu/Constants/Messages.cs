2d92ed6586ff8b5ef25eeca21affd73ddd5b13b6
halil ibrahim duymaz <halildymz16@hotmail.com>
2020-03-03 12:20:56 +0300

    Removed CannotGenerateMultiple constant

--- NLayeredContextMenu/Constants/Messages.cs
+++ NLayeredContextMenu/Constants/Messages.cs
@@ -9,7 +9,6 @@ namespace NLayeredContextMenu.Constants
     public class Messages
     {
         public static string ApplicationName = "DA & BL Oluşturma";
-        public static string CannotSupportMultipleCreation = "Henüz birden fazla Entity için oluşturma desteklenmemektedir.";
         public static string NoItemSelected = "Bu komutu çalıştırabilmek için bir nesne seçmelisiniz.";
         public static string MustBeImplementedFromIEntity = "Nesne IEntity implementasyonu olmak zorundadır.";
         public static string PleaseWait = "Lütfen Bekleyin";
